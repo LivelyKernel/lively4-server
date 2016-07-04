6faf394bb9f0b594308e7af205fa1e423b5812d3
Lixissimus <felix.wolff@student.hpi.uni-potsdam.de>
2016-07-04 17:55:38 +0200

    store indexable file extensions in a list

--- src/lunr-node-content-provider.js
+++ src/lunr-node-content-provider.js
@@ -4,6 +4,11 @@ var fs = require("fs");
 var slash = require("slash");
 var path = require("path");
 
+function isIndexable(filepath) {
+  let extentions = ["js", "html", "md"];
+  return extentions.indexOf(filepath.slice(filepath.lastIndexOf(".") + 1)) >= 0;
+}
+
 // this function throws an error if the index file does not exist
 export function loadIndexJson(l4idxFile) {
   fs.accessSync(l4idxFile, fs.R_OK | fs.W_OK);
@@ -26,7 +31,7 @@ function getFilepaths() {
         walk(path.join(rootDir, file));
       } else if (stat.isFile()) {
         // just index js-files for now, with size < 500kB
-        if ((file.slice(-3) === ".js" || file.slice(-5) === ".html" || file.slice(-3) === ".md") && stat.size < 500000) {
+        if (isIndexable(file) && stat.size < 500000) {
         // if (file.slice(-3) === ".js") {
           relFilePaths.push(path.join(rootDir, file));
         }
