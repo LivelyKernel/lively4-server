ae7112000515159223d8ab3ea4ee8684093795d3
Fabio Niephaus <code@fniephaus.com>
2016-07-06 20:56:54 +0200

    Add DELETE and PUT to CORS header

--- httpServer.js
+++ httpServer.js
@@ -412,7 +412,7 @@ http.createServer(function(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Request-Method', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, DELETE, PUT');
   res.setHeader('Access-Control-Allow-Headers', '*');
 
   var oUrl = url.parse(req.url, true, false);
