9aecf8223e3a82461eca5e7efe5111f4348dfa7c
Lively4 <lively4@lively-kernel.org>
2020-01-21 11:18:49 +0100

    new research server

---
+++ bin/lively4research.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LIVELY=~/research
+SERVER=~/lively4-server_research
+PORT=9007
+GITPULL=true
+LOGFILE=$SERVER/server.log
+AUTOCOMMIT=true
+AUTHORIZE=true
+ORGANIZATION=hpi-swa-lab
+TEAM=lively-research
+TEAM=BP2019RH1
+source $SERVER/bin/lively4server.sh
