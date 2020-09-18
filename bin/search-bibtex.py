c354c9df7986ef262fca159040965151f2681fc4
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2020-09-18 16:30:11 +0200

    search bib tex
    SQUASHED: AUTO-COMMIT-bin-search-bibtex-proxy.py,AUTO-COMMIT-bin-search-bibtex.py,

--- bin/search-bibtex.py
+++ bin/search-bibtex.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+#
+# Print Bibtex Entry for the first match of a given Scholar query:
+#
 
 import sys
 from scholarly import scholarly
 
+
 query = scholarly.search_pubs(sys.argv[1])
-pub = next(query)
+pub = next(query)       
+        
 print(pub.bibtex)
