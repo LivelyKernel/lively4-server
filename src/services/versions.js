5968032b1df9a692a0a0dc5eb5b459837fd2d42c
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2025-02-27 12:29:52 +0100

    docs: add JSDoc comments for VersionsService class and getVersion method

--- src/services/versions.js
+++ src/services/versions.js
@@ -2,8 +2,18 @@ import Service from "./service.js";
 import Path from 'path';
 import { run, respondWithCMD, fs_stat, logRequest, try_fs_stat, fs_exists, fs_readFile, fs_writeFile } from '../utils.js';
 
+/**
+ * Service class for handling version control operations
+ * @extends Service
+ */
 export default class VersionsService extends Service {
 
+  /**
+   * Gets the latest commit hash for a specific file in a repository
+   * @param {string} repositorypath - The path to the git repository
+   * @param {string} filepath - The path to the file within the repository
+   * @returns {Promise<string>} The commit hash of the latest commit for the file
+   */
   async getVersion(repositorypath, filepath) {
     return (await run(
       `cd "${repositorypath}"; git log -n 1 --pretty=format:%H -- "${filepath}"`
