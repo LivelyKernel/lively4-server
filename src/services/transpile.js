bd23f5194212760b265464e42b1727085c43fc8f
JensLincke <jens.lincke@hpi.uni-potsdam.de>
2025-02-27 12:34:34 +0100

    docs: add JSDoc comments for TranspileService class and its methods

--- src/services/transpile.js
+++ src/services/transpile.js
@@ -2,8 +2,19 @@ import Service from "./service.js";
 import Path from 'path';
 import { run, respondWithCMD, fs_stat, logRequest, try_fs_stat, fs_exists, fs_readFile, fs_writeFile } from '../utils.js';
 
+/**
+ * Service class for handling file transpilation operations
+ * @extends Service
+ */
 export default class TranspileService extends Service {
 
+  /**
+   * Invalidates (removes) transpiled files for a given source file
+   * @param {string} repositorypath - The path to the repository
+   * @param {string} filepath - The path to the source file
+   * @param {Object} req - The HTTP request object for logging
+   * @returns {Promise<void>}
+   */
   async invalidateTranspiledFile(repositorypath, filepath, req) {
     if (filepath.match(this.Config.transpileDir)) return  // don't do it on yourself
     if (!filepath.match(/\.js/)) return  // only javascript files are transpiled...
@@ -18,6 +29,12 @@ export default class TranspileService extends Service {
     logRequest(req, "RESULT " + result.stdout)
   }
 
+  /**
+   * Generates the path where a transpiled file should be stored
+   * @param {string} repositorypath - The path to the repository
+   * @param {string} filepath - The path to the source file
+   * @returns {string} The full path where the transpiled file should be stored
+   */
   transpilePath(repositorypath, filepath) {
     return repositorypath + "/" + this.Config.transpileDir + "/" + filepath.replace(/\//g, "_")
   }
